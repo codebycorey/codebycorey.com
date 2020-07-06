b4820fe545777b796ebc858d7656d1b5e6ce47c5
Corey O'Donnell <me@coreyodonnell.com>
2020-07-05 22:46:22 -0400

    Keep working on nextjs documentation

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,3 +1,19 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout'
+
 export default function FirstPost() {
-    return <h1> First Post</h1>
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1> First Post</h1>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+        </Layout>
+    )
 }
