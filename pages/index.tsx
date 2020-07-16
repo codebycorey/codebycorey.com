cc049e94f8e1f9f958a7a72799fc00a6285cbdcd
Corey O'Donnell <me@coreyodonnell.com>
2020-07-16 09:07:17 -0400

    Add styles to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,23 +10,30 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home({ allPostsData }: any) {
   return (
-    <div className="bg-gray-800 flex justify-center content-end h-screen">
+    <div className="bg-gray-900 flex justify-center content-end h-screen">
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <header className="flex justify-center content-center">
-        <div>
-          <img className="w-48 h-48" src="/images/profile.png" alt="Corey O'Donnell"/>
-          <h1>Corey O'Donnell</h1>
-          <h2>Full Stack Developer | Continuous Learner</h2>
-          <a href="">Blog</a>
-          <a href="">Uses</a>
-        </div>
-        <div>
-          <a href="https://github.com/CodeByCorey"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
-          <a href="https://twitter.com/codewithcorey"><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></a>
-          <a href="https://www.linkedin.com/in/rcodonnell/"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></a>
-          <a href="mailto:me@coreyodonnell.com"><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon></a>
+        <div className="flex flex-col justify-center content-center w-full">
+          <div className="flex w-full">
+            <img className="w-48 h-48 rounded-full border-2 border-green-500" src="/images/profile.png" alt="Corey O'Donnell"/>
+            <div className="pl-5 text-center">
+              <h1 className="text-gray-100 text-6xl">Corey O'Donnell</h1>
+              <h2 className="text-gray-500 text-4xl">Full Stack Developer</h2>
+              <h3 className="text-gray-500">Continuous Learner | Plant lover | Husband | Father</h3>
+            </div>
+          </div>
+          <div className="w-100 flex justify-center text-xl">
+            <a className="m-2 border-b-2 border-green-500">Blog</a>
+            <a className="m-2 border-b-2 border-green-500">Uses</a>
+          </div>
+          <div className="w-100 flex justify-center text-xl">
+            <a className="m-2 text-white" href="https://github.com/CodeByCorey"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
+            <a className="m-2 text-white" href="https://twitter.com/codewithcorey"><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></a>
+            <a className="m-2 text-white" href="https://www.linkedin.com/in/rcodonnell/"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></a>
+            <a className="m-2 text-white" href="mailto:me@coreyodonnell.com"><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon></a>
+          </div>
         </div>
       </header>
     </div>
