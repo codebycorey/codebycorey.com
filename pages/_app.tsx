313d0c9036d08bed2c4e71393cadb9142c48e122
Corey O'Donnell <me@coreyodonnell.com>
2020-08-07 00:39:25 -0400

    Add Fathom ID

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,34 +5,27 @@ import * as Fathom from 'fathom-client';
 
 import '../styles/global.css';
 
-const FATHOM_TRACKING: string | null = process.env.FATHOM || null;
-console.log('FATHOM_TRACKING', process.env.FATHOM);
 export default function App({ Component, pageProps }: AppProps) {
 
   const router: NextRouter = useRouter();
 
   useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load('ILBUCPDU', {
+      includedDomains: ['coreyodonnell.dev']
+    });
 
     function onRouteChangeComplete() {
       Fathom.trackPageview();
     }
 
-    console.log('code', FATHOM_TRACKING);
-    if (FATHOM_TRACKING) {
-      // Initialize Fathom when the app loads
-      Fathom.load(FATHOM_TRACKING, {
-        includedDomains: ['https://coreyodonnell.dev']
-      });
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
 
-      // Record a pageview when route changes
-      router.events.on('routeChangeComplete', onRouteChangeComplete);
-
-      // Unassign event listener
-      return () => {
-        router.events.off('routeChangeComplete', onRouteChangeComplete);
-      };
-
-    }
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
   });
   return <Component {...pageProps} />
 }
