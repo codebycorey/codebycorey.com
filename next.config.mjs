ef3bfc132146688c19f524b6754496705c76f109
codebycorey <me@coreyodonnell.com>
2024-05-08 16:57:14 -0400

    fix analytics

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.usefathom.com;
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
